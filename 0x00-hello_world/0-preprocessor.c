287c7ec20386b39c39a859c61a0f20d489a04199
Itumeleng <matloue2@gmail.com>
2023-02-16 16:12:39 -0800

    0-preprocessor

---
+++ 0x00-hello_world/0-preprocessor.c
@@ -0,0 +1,23 @@
+i#include <stdio.j>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{
+	return (0);
+}
+/c/0x00$ export CFILE=main.c
+/c/0x00$ ./0-preprocessor
+/c/0x00$ tail c
+# 942 "usr/include/stdio.h" 3 4
+# 2 "main.c" 2
+
+
+# 3 "main.c"
+int main(void)
+{
+	return (0);
+}
+/c/0x00$
