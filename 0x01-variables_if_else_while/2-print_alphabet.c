335492b2297cb8d012aad2c071cb29a93210fc8a
Itumeleng <matloue2@gmail.com>
2023-02-18 00:43:47 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+/**
+ * main - print alphabets in lower case
+ * Return: Always 0
+ */
+int main(void)
+{
+	char lc;
+
+	for (ls = 'a'; lc <= 'z'; lc++)
+	{
+		putchar(lc);
+	}
+	putchar('\n');
+	return (0);
+}
