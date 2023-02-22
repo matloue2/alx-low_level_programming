a68259e317ef6394a3ef7230618ee187bc6dd953
Itumeleng <matloue2@gmail.com>
2023-02-21 17:12:06 -0800

    update 2

--- 0x02-functions_nested_loops/2-main.c
+++ 0x02-functions_nested_loops/2-main.c
@@ -1,23 +1,12 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 -Make alphabet x10 times
- * Return: void
+ * main - check the code.
+ *
+ * Return: Always 0.
  */
-
-void print_alphabet_x10(void)
+int main(void)
 {
-
-	char c;
-	int i = 0;
-
-	while (i <= 9)
-	{
-	for (c = 'a'; c <= 'z'; c++)
-	{
-	putchar (c);
-	}
-	_putchar('\n');
-	i++;
-	}
+    print_alphabet_x10();
+    return (0);
 }
