88708bf3848dd722494e16b6844ea45e899caf02
Itumeleng <matloue2@gmail.com>
2023-02-24 16:59:57 -0800

    3-print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include"main.h"
+
+/**
+ * print_numbers - print 0 - 9
+ *             only using _putchar twice
+ *
+ * Return: Nothing
+ */
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	do {
+		_putchar(i + 48);
+		i++;
+	} while (i >= 0 && i <= 9);
+	_putchar('\n');
+}
