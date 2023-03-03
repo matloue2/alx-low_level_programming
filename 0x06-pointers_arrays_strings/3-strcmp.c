8871c2631c1dadcdb2d86405b7814cd58af1db44
Itumeleng <matloue2@gmail.com>
2023-03-02 19:30:31 -0800

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,24 +1,39 @@
 #include "main.h"
 
 /**
- * _strcmp - compare string values
- * @s1: input value
- * @s2: input value
- *
- * Return: s1[i] - s2[i]
+ * cap_string - capitalizes all words of a string
+ * @str: pointer to the string to capitalize
+ * Return: pointer to the capitalized string  `str`
  */
-int_strcmp(char *s1, char *s2)
-{
-	int i;
 
-	i = 0;
-	while (s1[i] != '\0' && s2[i] != '\0')
+char *cap_string(char *str)
+{
+	int i, c;
+	int is_word_start;
+	char nots[] = ",;.!?(){}\n\t\" ";
+
+	for (i = 0, is_word_start = 0; str[i] != '\0'; i++)
 	{
-		if (s1[i] != s2[i])
+		if (str[0] > 96 && str[0] < 123)
+			is_word_start = 1;
+		for (c = 0; nots[c] != '\0'; c++)
 		{
-		return (s1[i] - s2[i]);
+			if (nots[c] == str[i])
+				is_word_start = 1;
 		}
-		i++;
+
+		if (is_word_start)
+		{
+			if (str[i] > 96 && str[i] < 123)
+			{
+				str[i] -= 32;
+				is_word_start = 0;
+			}
+			else if (str[i] > 64 && str[i] < 91)
+				is_word_start = 0;
+			else if (str[i] > 47 && str[i] < 58)
+				is_word_start = 0;
+		}
 	}
-	return (0);
+	return (str);
 }
