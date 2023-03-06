44d3a29eb9b1395cf9d79d3b655a0ff546350bec
Itumeleng <matloue2@gmail.com>
2023-03-06 15:24:49 -0800

    update

--- 0x07-pointers_arrays_strings/0-main.c
+++ 0x07-pointers_arrays_strings/0-main.c
@@ -10,24 +10,25 @@
  */
 void simple_print_buffer(char *buffer, unsigned int size)
 {
-	unsigned int i;
+        unsigned int i;
 
-	i = 0;
-	while (i < size)
-	{
-		if (i % 10)
-		{
-			printf(" ");
-		}
-					if (!(i % 10) && i)
-			{
-					printf("\n");
-			}
-					printf("0x%02x", buffer[i]);
-					i++;
-	}
-					printf("\n");
+        i = 0;
+        while (i < size)
+        {
+                if (i % 10)
+                {
+                        printf(" ");
+                }
+                if (!(i % 10) && i)
+                {
+                        printf("\n");
+                }
+                printf("0x%02x", buffer[i]);
+                i++;
+        }
+        printf("\n");
 }
+
 /**
  * main - check the code
  *
