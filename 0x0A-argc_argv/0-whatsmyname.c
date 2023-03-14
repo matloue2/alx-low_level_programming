17404749ce90efa548d5759fa3cfc059c8f996c3
Itumeleng <matloue2@gmail.com>
2023-03-14 07:05:57 -0700

    0-1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: The number of arguments used in the main function
+ * @argv: An array  of pointers to the arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
